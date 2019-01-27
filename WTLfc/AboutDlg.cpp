f5f7b732126580312e7a06f20f793c5425c8efac
name2017 <name2017@163.com>
2019-01-27 21:26:30 +0800

    WTLfc_VS2017-Commit07.05-更新版本号为WTL_Freecell v1.3.0.2 20190126。解决Release版在Win10高DPI中的窗口大小显示问题

--- WTLfc/AboutDlg.cpp
+++ WTLfc/AboutDlg.cpp
@@ -45,7 +45,7 @@ LRESULT CAboutDlg::OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lPara
 	//::SetWindowText(m_edit1.m_hWnd, "This a multi test.");
 	::SetWindowText(m_edit1.m_hWnd,
 		L"    --------------------------------------------\r\n"
-		L"    WTL_Freecell v1.2 (WTL空当接龙v1.2) 20180112\r\n"
+		L"    WTL_Freecell v1.3 (WTL空当接龙v1.3) 20190126\r\n"
 		L"    --------------------------------------------\r\n"
 		L"    By     ：MinJun\r\n"
 		L"    QQ     ：153248043\r\n"
